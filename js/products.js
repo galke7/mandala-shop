be106f092ec600cdfdbb00a2772747d1a4899e09
galke7 <gal.kerem@gmail.com>
2026-06-09 10:03:33 +0300

    Add product: מנדלה לבנה 35 ס״מ

--- js/products.js
+++ js/products.js
@@ -8,6 +8,7 @@ export const products = [
   { id: 'p6', name: 'מנדלה ענקית מהממת 60 ס״מ', price: 400, image: 'images/60cm-IMG_20260605_142604.jpg', desc: 'מנדלה ענקית מהממת סרוגה בעבודת יד, קוטר כ-60 ס״מ.' },
   { id: 'p7', name: 'מנדלת פתיתי שלג 35 ס״מ', price: 320, image: 'images/35cm-p7.jpg', desc: 'מנדלת פתיתי שלג סרוגה בעבודת יד, קוטר כ-35 ס״מ.' },
   { id: 'p8', name: 'מנדלת ענווה 30 ס״מ', price: 300, image: 'images/30cm-p8.jpg', desc: 'מנדלת ענווה סרוגה בעבודת יד, קוטר כ-30 ס״מ. זמין במלאי.' },
+  { id: 'p9', name: 'מנדלה לבנה 35 ס״מ', price: 320, image: 'images/35cm-p9.jpg', desc: 'מנדלה לבנה סרוגה בעבודת יד, קוטר כ-35 ס״מ.' },
 ];
 
 export function findProduct(id) {
