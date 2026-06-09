44d191beb149e0e53c542d2c843a3763cb2d981e
galke7 <gal.kerem@gmail.com>
2026-06-09 14:40:15 +0300

    Add product: מנדלת שמחה 50 ס״מ

--- js/products.js
+++ js/products.js
@@ -10,6 +10,7 @@ export const products = [
   { id: 'p8', name: 'מנדלת ענווה 30 ס״מ', price: 300, image: 'images/30cm-p8.jpg', desc: 'מנדלת ענווה סרוגה בעבודת יד, קוטר כ-30 ס״מ. זמין במלאי.' },
   { id: 'p9', name: 'מנדלה לבנה 35 ס״מ', price: 320, image: 'images/35cm-p9.jpg', desc: 'מנדלה לבנה סרוגה בעבודת יד, קוטר כ-35 ס״מ.' },
   { id: 'p10', name: 'מנדלת שמחה 50 ס״מ', price: 400, image: 'images/50cm-p10.jpg', desc: 'מנדלת שמחה סרוגה בעבודת יד, קוטר כ-50 ס״מ.' },
+  { id: 'p11', name: 'מנדלת שמחה 50 ס״מ', price: 400, image: 'images/50cm-p11.jpg', desc: 'מנדלת שמחה סרוגה בעבודת יד, קוטר כ-50 ס״מ.' },
 ];
 
 export function findProduct(id) {
